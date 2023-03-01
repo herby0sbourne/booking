044120b845c6c2cab2eb8a14614ba7326054ccb6
Herby <herbyosbourne31@gmail.com>
2023-03-01 13:40:41 -0500

    create post Route for search-availability

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -23,6 +23,7 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/generals-quarters", handlers.Repo.Generals)
 	mux.Get("/make-reservation", handlers.Repo.Reservation)
 	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Post("/search-availability", handlers.Repo.PostAvailability)
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
