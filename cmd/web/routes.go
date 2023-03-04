565ba58242eb64f79ee99167d12ec854d2bc1da2
Herby <herbyosbourne31@gmail.com>
2023-03-04 09:05:31 -0500

    add PostReservation route handler

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/herby0sbourne/booking/internal/config"
 	"github.com/herby0sbourne/booking/internal/handlers"
-	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -21,7 +22,9 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/contact", handlers.Repo.Contact)
 	mux.Get("/majors-suite", handlers.Repo.Majors)
 	mux.Get("/generals-quarters", handlers.Repo.Generals)
+
 	mux.Get("/make-reservation", handlers.Repo.Reservation)
+	mux.Post("/make-reservation", handlers.Repo.PostReservation)
 
 	mux.Get("/search-availability", handlers.Repo.Availability)
 	mux.Post("/search-availability", handlers.Repo.PostAvailability)
